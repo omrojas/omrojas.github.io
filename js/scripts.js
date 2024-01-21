6d8edf6019645438766d40321f572b809e5f9627
Omar Martínez <omar.ro@outlook.com>
2024-01-20 18:53:37 -0600

    More errors

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ function filterObjects(c){
     var x, i;
     x = document.getElementsByClassName("box");
     if (c == "all") c = "";
-    for (i = 0; i <, x.length; i++) {
+    for (i = 0; i < x.length; i++) {
         removeClass(x[i], "show");
         if(x[i].className.indexOf(c) > -1) addClass(x[i], "show")
     }
